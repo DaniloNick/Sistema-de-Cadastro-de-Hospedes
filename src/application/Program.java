aa433a84eb21f32d46d9d0b78e3d6fb0ac004097
DaniloNick <daniloageu@gmail.com>
2022-11-28 06:40:24 -0300

    creation of guest registration system

---
+++ src/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+import entities.Cadastro;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bem vindo, a pensao possui 10 quartos, quantos alunos irao se hospedar? ");
+        int n = sc.nextInt();
+
+        if (n < 1 || n > 10) {
+            do {
+                System.out.println("Por favor, digite uma quantidade valida!");
+                System.out.println("Quantos alunos irao se hospedar ?");
+                n = sc.nextInt();
+            } while (n > 10 || n < 1);
+        }
+
+        Cadastro[] vetor = new Cadastro[n + 1];
+
+        for (int i = 1; i < vetor.length; i++) {
+            sc.nextLine();
+            System.out.println("Ola, qual o seu nome ?");
+            String nome = sc.nextLine();
+            System.out.println("Qual o seu email?");
+            String email = sc.next();
+            System.out.println("Digite o numero do quarto desejado: ");
+            int numQuarto = sc.nextInt();
+            vetor[i] = new Cadastro(nome, email, numQuarto);
+            System.out.println("Cadastro Realizado");
+            System.out.println();
+        }
+        System.out.println("Quartos Reservados: ");
+        System.out.println();
+        String hospede;
+        for (int i = 1; i < n; i++) {
+            hospede = vetor[i].toString();
+            System.out.println(hospede);
+        }
+        sc.close();
+    }
+}
+
---
+++ src/entities/Cadastro.java
@@ -0,0 +1,43 @@
+package entities;
+
+       public class Cadastro {
+        String nome;
+        String email;
+        int numQuarto;
+
+        public Cadastro(String nome, String email, int numQuarto) {
+            this.nome = nome;
+            this.email = email;
+            this.numQuarto = numQuarto;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public int getNumQuarto() {
+            return numQuarto;
+        }
+
+        public void setNumQuarto(int numQuarto) {
+            this.numQuarto = numQuarto;
+        }
+
+        public String toString() {
+            return "Nome: " + nome + "\n" + "Email: " + email + "\n" + "Quarto: N " + numQuarto + "\n";
+        }
+
+    }
+
